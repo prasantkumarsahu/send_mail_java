4f4755bc76a70e00cb0cf02b99ce5ff69591c284
Prasant Kumar Sahu <prasantkumarsahu0103@gmail.com>
2023-04-16 13:35:33 +0530

    fix

--- src/main/java/com/prasant/SendMailJava/MailAuthenticator.java
+++ src/main/java/com/prasant/SendMailJava/MailAuthenticator.java
@@ -8,7 +8,7 @@ public class MailAuthenticator extends Authenticator {
 	@Override
 	protected PasswordAuthentication getPasswordAuthentication() {
 		
-		return new PasswordAuthentication(MailConstants.SENDER, "0prasant*kumar0sahu*");
+		return new PasswordAuthentication(MailConstants.SENDER, "emiocszwhoatuzwp");
 	}
 	
 	
